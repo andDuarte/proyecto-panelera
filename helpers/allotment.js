81f1269594097e89d7369a7b9701d6bbb67dee13
andDuarte <nuevo.sena23@gmail.com>
2023-05-01 17:14:53 -0500

    add validations in the model, route and controller allotment

---
+++ helpers/allotment.js
@@ -0,0 +1,13 @@
+import { allotmentModel } from '../models/allotment.js';
+
+const allotmentValidate = {
+    allotmentId: async(id) => {
+        const allotment = await allotmentModel.find({_id: id});
+
+        if(allotment.length == 0) {
+            throw new Error('params id no existe en la base de datos');
+        }
+    },
+}
+
+export{ allotmentValidate }
--- routes/allotment.js
+++ routes/allotment.js
@@ -2,16 +2,48 @@ import { Router } from 'express';
 
 import { allotmentHttp } from '../controllers/allotment.js';
 
+import { check } from 'express-validator';
+
+import { validate } from '../middlewares/validate-fields.js';
+
+import { validateToken } from '../middlewares/validate-jwt.js';
+
+import { allotmentValidate } from '../helpers/allotment.js';
+
 const routerAllotment = Router();
 
 routerAllotment.get('/', [], allotmentHttp.allotmentGet );
 
-routerAllotment.post('/', [], allotmentHttp.allotmentPost );
+routerAllotment.post('/', [
+    check('owner', 'body dueño del lote es necesario').trim().notEmpty(),
+    check('size', 'body tamaño es necesario').trim().notEmpty(),
+    check('token', 'header token es necesario').trim().notEmpty(),
+    check('token').custom(validateToken),
+    validate
+], allotmentHttp.allotmentPost );
 
-routerAllotment.put('/:id', [], allotmentHttp.allotmentPut );
+routerAllotment.put('/:id', [
+    check('id', 'id no valido').isMongoId(),
+    check('id').custom(allotmentValidate.allotmentId),
+    check('token', 'header token es necesario').trim().notEmpty(),
+    check('token').custom(validateToken),
+    validate
+], allotmentHttp.allotmentPut );
 
-routerAllotment.put('/activar/:id', [], allotmentHttp.allotmentActivate );
+routerAllotment.put('/activar/:id', [
+    check('id', 'params id no valido').isMongoId(),
+    check('id').custom(allotmentValidate.allotmentId),
+    check('token', 'header token es necesario').trim().notEmpty(),
+    check('token').custom(validateToken),
+    validate
+], allotmentHttp.allotmentActivate );
 
-routerAllotment.put('/desactivar/:id', [], allotmentHttp.allotmentDesactivate );
+routerAllotment.put('/desactivar/:id', [
+    check('id', 'params id no valido').isMongoId(),
+    check('id').custom(allotmentValidate.allotmentId),
+    check('token', 'header token es necesario').trim().notEmpty(),
+    check('token').custom(validateToken),
+    validate
+], allotmentHttp.allotmentDesactivate );
 
 export{ routerAllotment }
